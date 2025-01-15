9ed29fffbcc12ea052b6f6f5a9be3ead4751c352
Alexander Garyaev <125agaryaev@frhsd.com>
2025-01-15 12:53:56 -0500

    working on it

--- games/src/WeatherApp.tsx
+++ games/src/WeatherApp.tsx
@@ -4,18 +4,6 @@ import './index.css';
 import App from './App';
 import {HashRouter, Routes, Route, Outlet} from "react-router";
 
-function Display({name, pfp}: {name: string, pfp: string})
-{
-    return(
-        <>
-            <div>
-                <img src = {pfp} alt = "profilepic"/>
-                <p>{name}</p>
-            </div>
-        </>
-    )
-}
-
 export function WeatherApp()
 {
     const [addresses, setAddresses]: [any[], any] = useState([]);
@@ -31,34 +19,9 @@ export function WeatherApp()
             .catch((error) => console.log(error));
     }, [address]);
 
-
-    function Display2()
-    {
-        setAddresses(test)
-    }
-
-    function Display3()
-    {
-        return(
-            <>
-                <div>
-                    <p>Forecast for</p>
-                    <ul>
-                        {addresses && addresses.map(address => (<Display name={address.login} pfp={address.name}></Display>))}
-                    </ul>
-                </div>
-            </>)
-    }
-
     return(
         <>
-            <div>
-                <h1> Weather App</h1>
-                <input type = "text" onChange={(e) => setTest(e.target.value)}></input>
-                <button onClick = {Display2}>Search</button>
-                <p></p>
-                <Display3></Display3>
-            </div>
+
         </>
     )
 }
