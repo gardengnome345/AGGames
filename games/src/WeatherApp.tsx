1f20ca6f1df7dc062d082ec51d204848cd5eabb0
Alexander Garyaev <125agaryaev@frhsd.com>
2025-01-21 12:18:10 -0500

    working on it

--- games/src/WeatherApp.tsx
+++ games/src/WeatherApp.tsx
@@ -6,14 +6,23 @@ import {HashRouter, Routes, Route, Outlet} from "react-router";
 
 export function WeatherApp()
 {
-    const [addresses, setAddresses]: [any[], any] = useState([]);
-    const [address] = useState("");
-    const [test, setTest] = useState("");
+    const [address, setAddress]: [any[], any] = useState("2 Roberstville Rd, Freehold NJ");
+    const [cords, setCords] = useState({x: 0, y: 0});
+    const [api, setApi] = useState("");
+    const [period, setPeriod] = useState< {
+        mainForecast: string;
+        shortForecast: string;
+        icon: string;
+        name: string;
+        chanceOfPrecip: string;
+        temp: number;
+    }[]>([]);
+
     useEffect(() => {
         fetch(`https://api.geoapify.com/v1/geocode/search?text=${address}&format=json&apiKey=b8568cb9afc64fad861a69edbddb2658`)
             .then((response) => response.json())
             .then((data) => {
-                setAddresses(data.items);
+                setAddress(data.items);
                 console.log(data);
             })
             .catch((error) => console.log(error));
