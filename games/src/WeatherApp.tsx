515e3d7e0b727c583b01770452a184cb88e87d35
Alexander Garyaev <125agaryaev@frhsd.com>
2025-01-22 14:05:29 -0500

    working on it

--- games/src/WeatherApp.tsx
+++ games/src/WeatherApp.tsx
@@ -6,33 +6,54 @@ import {HashRouter, Routes, Route, Outlet} from "react-router";
 
 export function WeatherApp()
 {
-    const [address, setAddress]: [any[], any] = useState("2 Roberstville Rd, Freehold NJ");
+    const [address, setAddress] = useState("2 Robertsville Rd, Freehold, NJ");
     const [cords, setCords] = useState({x: 0, y: 0});
     const [api, setApi] = useState("");
-    const [period, setPeriod] = useState< {
-        mainForecast: string;
-        shortForecast: string;
-        icon: string;
-        name: string;
-        chanceOfPrecip: string;
-        temp: number;
-    }[]>([]);
+    const [period, setPeriod] = useState<any[]>([]);
 
     useEffect(() => {
         fetch(`https://api.geoapify.com/v1/geocode/search?text=${address}&format=json&apiKey=b8568cb9afc64fad861a69edbddb2658`)
             .then((response) => response.json())
             .then((data) => {
-                setAddress(data.items);
-                console.log(data);
+               const a =
+                   {
+                       x: data.results[0].lat,
+                       y: data.results[0].lon
+                   }
+                   setCords(a)
             })
             .catch((error) => console.log(error));
     }, [address]);
+
+    useEffect(() => {
+        fetch("https://api.weather.gov/points/" + cords.x + "%2C" + cords.y + "")
+            .then((response) => response.json())
+            .then((period) => {
+                setApi(period.properties.forecast);
+            })
+    }, [cords]);
+
+    useEffect(() => {
+        fetch(api)
+        .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setPeriod(data.properties.period);
+            })
+            .catch((error) => console.log(error));
+    }, [api]);
+    document.body.style.backgroundColor = "white";
 
     return(
         <>
 
         </>
     )
+}
+
+function Searchbar()
+{
+
 }
 
 
